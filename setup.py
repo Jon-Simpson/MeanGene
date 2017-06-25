21b5a070c7af7f0804f9385ea6bde15d960e0c80
Jonathan Simpson <simpsonjd@g.cofc.edu>
2017-06-25 19:55:21 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='MeanGene',
+    version='1.0',
+    packages=['MeanGene'],
+    url='https://github.com/Jon-Simpson/MeanGene',
+    author='jonsimpson',
+    author_email='jondsimp@gmail.com',
+    description='Clustering Tool for Genetic Expression Analysis'
+)
